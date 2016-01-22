b6f525813de05dc09edae2fce9405ad3b6204b17
Abola Lee <abola921@gmail.com>
2016-01-22 21:48:02 +0800

    Update Ch3Coz1.java

--- jcconf2015/Ch3Coz1.java
+++ jcconf2015/Ch3Coz1.java
@@ -12,7 +12,7 @@ public class Ch3Coz1 {
         String api = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=55ec6d6e-dc5c-4268-a725-d04cc262172b";
 
         // call remote api
-        CrawlerPack.getFromRemote(api);
+        CrawlerPack.start().getFromJson(api);
 
     }
 }
