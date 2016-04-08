1e7cfbb8961a9350a4f6165b2af4659cde271e29
Abola Lee <abola921@gmail.com>
2016-04-08 13:59:54 +0800

    include junit4 and some test class

---
+++ src/test/java/com/github/abola/crawler/CrawlerPackInitTest.java
@@ -0,0 +1,19 @@
+package com.github.abola.crawler;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by abolalee on 2016/4/8.
+ */
+public class CrawlerPackInitTest {
+
+    @Test
+    public void startCrawlerInstance(){
+        try {
+            CrawlerPack.start();
+        }catch ( Exception anyException ){
+            Assert.fail( "Create CrawlerPack instance fail. Cause by: " + anyException.getMessage() );
+        }
+    }
+}
